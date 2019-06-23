509d5d29b0c83eec441e24b5fce796b294912fe0
nkmr-jp <nkmr.git@gmail.com>
2019-06-23 23:36:32 +0900

    :art: var -> const

--- Github_code_translate_ignore.user.js
+++ Github_code_translate_ignore.user.js
@@ -8,11 +8,11 @@
 
 (function () {
   'use strict';
-  var preTags = document.querySelectorAll('.highlight pre');
+  const preTags = document.querySelectorAll('.highlight pre');
   for (let i = 0; i < preTags.length; i++) {
-    var preTag = preTags[i];
-    var codeTag = document.createElement('code');
-    codeTag.style.cssText="padding:0"
+    const preTag = preTags[i];
+    const codeTag = document.createElement('code');
+    codeTag.style.cssText = 'padding:0'
     preTag.after(codeTag);
     codeTag.append(preTag);
   }
