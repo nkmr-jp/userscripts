2a67c23cf9420832f64f149d77a44001ffc1718c
nkmr-jp <nkmr.git@gmail.com>
2019-06-29 23:44:59 +0900

    :sparkles: Multiple site support

--- Translate_excluding_code/README.md
+++ Translate_excluding_code/README.md
@@ -1,7 +1,7 @@
-# Elasticsearch Translate excluding code
+# Translate excluding code
 
 Translate excluding code blocks when translating pages with Google Translate.
 
 1. Add Chrome Extention [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
-2. [Install](https://github.com/nkmr-jp/userscripts/raw/master/Elasticsearch_Translate_excluding_code/script.user.js). 
+2. [Install](https://github.com/nkmr-jp/userscripts/raw/master/Translate_excluding_code/script.user.js). 
 
--- Translate_excluding_code/script.user.js
+++ Translate_excluding_code/script.user.js
@@ -1,21 +1,19 @@
 // ==UserScript==
-// @name         Elasticsearch Translate excluding code
+// @name         Translate excluding code
 // @namespace    https://github.com/nkmr-jp/userscripts
 // @description  Translate excluding code blocks when translating pages with Google Translate.
 // @author       nkmr-jp
+// @include      https://github.com/*
 // @include      https://www.elastic.co/*
+// @include      https://medium.com/*
 // ==/UserScript==
-
-const SELECTORS = '.pre_wrapper pre';
 
 (function () {
   'use strict';
-  const targets = document.querySelectorAll(SELECTORS);
+  const targets = document.querySelectorAll('pre');
   for (let i = 0; i < targets.length; i++) {
     const target = targets[i];
-    const wrapper = document.createElement('code');
-    wrapper.style.cssText = 'padding:0'
-    target.after(wrapper);
-    wrapper.append(target);
+    const org_html = target.innerHTML;
+    target.innerHTML = `<code style="padding:0">${org_html}</code>`;
   }
 })();
