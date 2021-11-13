76e913b2ea483b20116acd3698e64eca22e8007f
nkmr-jp <nkmr-jp@users.noreply.github.com>
2021-11-13 15:36:38 +0900

    Update script.user.js

--- Translate_excluding_code/script.user.js
+++ Translate_excluding_code/script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Translate excluding code
-// @version      0.4
+// @version      0.5
 // @namespace    https://github.com/nkmr-jp/userscripts
 // @description  Translate excluding code blocks when translating pages with Google Translate.
 // @author       nkmr-jp
